7164d21fe7a3e685e0f65ab78e4babb42e4acfa8
Ravi Kumar <rravikumar27316@gmail.com>
2024-09-10 13:12:33 +0530

    add hero

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -9,9 +9,9 @@ function Hero() {
           <img
             src={hero}
             alt="Technoloyg Pre-incubation Cell"
-            className="absolute w-full h-[90vh] object-cover"
+            className="absolute w-full h-[95vh] object-cover"
           />
-          <div className="relative h-[90vh] [background:linear-gradient(0.62deg,_rgba(255,_250,_250,_0),_rgba(56,_55,_55,_0.78)_34.95%,_rgba(16,_16,_16,_0.94)_50%,_rgba(10,_9,_9,_0.96)_75.04%,_#000)] inset-0 flex justify-center sm:justify-center items-center text-center px-[16px]">
+          <div className="relative h-[95vh] [background:linear-gradient(0.62deg,_rgba(255,_250,_250,_0),_rgba(56,_55,_55,_0.78)_34.95%,_rgba(16,_16,_16,_0.94)_50%,_rgba(10,_9,_9,_0.96)_75.04%,_#000)] inset-0 flex justify-center sm:justify-center items-center text-center px-[16px]">
             <div class="w-full flex flex-wrap flex-col justify-center items-center text-[#F2F0EF] mx-auto max-w-screen-lg">
               <h1 className="text-[64px] sm:text-[80px] md:text-[120px] font-extrabold font-SFPro text-base3">
                 TPIC <span className="text-white">HNBGU</span>
