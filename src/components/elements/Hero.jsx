d3a2cc1416eafe0dbbcbb31bc925fd8ebbbcafa0
Ravi Kumar <rravikumar27316@gmail.com>
2024-09-10 13:14:43 +0530

    add hero

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -13,10 +13,10 @@ function Hero() {
           />
           <div className="relative h-[95vh] [background:linear-gradient(0.62deg,_rgba(255,_250,_250,_0),_rgba(56,_55,_55,_0.78)_34.95%,_rgba(16,_16,_16,_0.94)_50%,_rgba(10,_9,_9,_0.96)_75.04%,_#000)] inset-0 flex justify-center sm:justify-center items-center text-center px-[16px]">
             <div class="w-full flex flex-wrap flex-col justify-center items-center text-[#F2F0EF] mx-auto max-w-screen-lg">
-              <h1 className="text-[64px] sm:text-[80px] md:text-[120px] font-extrabold font-SFPro text-base3">
+              <h1 className="text-[56px] sm:text-[80px] md:text-[120px] font-extrabold font-SFPro text-base3">
                 TPIC <span className="text-white">HNBGU</span>
               </h1>
-              <h2 className="text-base3/60 text-[22px] sm:text-[28px] md:text-[42px] -mt-6 md:-mt-10">
+              <h2 className="text-base3/60 text-[20px] sm:text-[28px] md:text-[42px] -mt-4 md:-mt-10">
                 Technology Pre-Incubation Cell HNBGU
               </h2>
               <p className="text-white text-[20px] sm:text-[40px] font-semibold tracking-wide mt-8">
