9004ffbe15f22cfcd4e079a6b431adf1ea8f63fc
Ravi Kumar <rravikumar27316@gmail.com>
2024-10-08 19:45:22 +0530

    add nodes

--- src/components/important/Tech.jsx
+++ src/components/important/Tech.jsx
@@ -13,7 +13,7 @@ function Tech() {
       </div>
       {/* <img src={team} alt="statrtup" width={1200} height={1200} className="w-[360px] items-center" /> */}
       <p className='mt-10 text-white text-justify text-[14px] sm:text-[18px] -tracking-tight leading-[135%]'>
-      Research and Development plays a vital role in a Technology Pre-Incubation Cell by fostering innovation, providing technical guidance, and developing proof-of-concept solutions. It helps identify viable ideas, conduct feasibility studies, and create prototypes that enhance the potential of early-stage startups. R&D also aids in securing intellectual property, reducing technical risks, and aligning innovations with market trends. By offering access to technical expertise, advanced tools, and collaborative networks, R&D helps startups refine their products and business models, making them more competitive and increasing their chances of success during the incubation phase.
+      Research and Development plays a vital role in a Technology Pre-Incubation Cell by fostering innovation, providing technical guidance, and developing proof-of-concept solutions. It helps identify viable ideas, conduct feasibility studies, and create prototypes that enhance the potential of early-stage startups. R&D also aids in securing intellectual property, reducing technical risks, and aligning innovations with market trends. By offering access to technical expertise, advanced tools, and collaborative networks, R&D helps startups refine their products and business models, making them more competitive and increasing their chances of success during the incubation phase.
       </p>
       </div>
       <div className='mt-12 flex flex-wrap flex-col w-full justify-center items-center '>
