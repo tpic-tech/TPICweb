fd976c31aded4e7b511eb16a902a24c0349e7883
tpic-tech <tpichnbgu@gmail.com>
2025-07-09 15:23:55 +0530

    Update Gallery.jsx

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-
-const imageFilenames = ["IMG1.jpeg", "IMG2.jpeg", "IMG3.jpeg"];
-
+import ContentAwaited from "../../components/elements/ContentAwaited";
 function Gallery() {
   return (
-    <div className="max-w-screen-lg mx-auto flex flex-wrap justify-center items-center gap-6 mt-10 px-6 mb-20">
-      {imageFilenames.map((filename, index) => (
-        <img
-          key={index}
-          src={`/images/events/${filename}`}  // reference from public/
-          alt={`Event ${index + 1}`}
-          className="w-72 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
-        />
-      ))}
-    </div>
+    <>
+      <div className="max-w-screen-lg mx-auto flex flex-wrap justify-center item-center mt-10 px-6 mb-20">
+        <ContentAwaited />
+      </div>
+    </>
   );
 }
 
 export default Gallery;
-
